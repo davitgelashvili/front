b46da35b4e54c1fd18bdc1617f3db668e6930bd6
Davit Gelashvili <datoogelashvili@gmail.com>
2026-06-12 16:49:20 +0400

    change

--- src/features/batch/add/Add.jsx
+++ src/features/batch/add/Add.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react'
 import { useNavigate, useParams } from 'react-router-dom'
 import { useAuth } from '../../../context/AuthContext'
 import useApi from '../../../http/useApi'
-import Form from './form'
+import Form from './Form'
 import { useToast } from '../../../context/ToastContext'
 
 export default function AddBatch() {
